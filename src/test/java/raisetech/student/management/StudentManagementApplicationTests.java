76f01f6a108c576ec52f9ec6ecf14f2f4e08992e
yuik23 <yui_karamatsu@yahoo.co.jp>
2025-01-25 16:38:47 +0900

    first commit

---
+++ src/test/java/raisetech/student/management/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.student.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
