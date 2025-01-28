a9f578a23a88d520c1939eaa61d7ef5605a27ef1
yuik23 <yui_karamatsu@yahoo.co.jp>
2025-01-29 05:59:42 +0900

    Add CRUD

---
+++ src/main/java/raisetech/student/management/Application.java
@@ -0,0 +1,44 @@
+package raisetech.student.management;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+  @Autowired
+  private StudentRepository repository;
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @GetMapping("/student")
+  public String getStudent(@RequestParam String name) {
+    Student student = repository.searchByName(name);
+    return student.getName() + " " + student.getAge() + "歳";
+  }
+
+  @PostMapping("/student")
+  public void registerStudent(String name, int age) {
+    repository.registerStudent(name, age);
+  }
+
+  @PatchMapping("/student")
+  public void setName(String name, int age) {
+    repository.updateStudent(name, age);
+  }
+
+  @DeleteMapping("/student")
+  public void deleteStudent(String name) {
+    repository.deleteStudent(name);
+  }
+}
