d68a40c5e5b0ad3e08e641af54166adbc9a875bd
yuik23 <yui_karamatsu@yahoo.co.jp>
2025-03-23 18:30:32 +0900

    StudentServiceTestの実装

--- src/test/java/raisetech/student/management/service/StudentServiceTest.java
+++ src/test/java/raisetech/student/management/service/StudentServiceTest.java
@@ -1,11 +1,19 @@
 package raisetech.student.management.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +22,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import raisetech.student.management.controller.converter.StudentConverter;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentCourse;
+import raisetech.student.management.domain.StudentDetail;
+import raisetech.student.management.exception.MemberNotFoundException;
 import raisetech.student.management.repository.StudentRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -47,4 +57,77 @@ class StudentServiceTest {
     verify(converter, times(1)).convertStudentDetails(studentList, studentCourseList);
   }
 
+  @Test
+  void IDによる受講生詳細検索_IDが存在する場合にリポジトリの処理が適切に呼び出せていること()
+      throws MemberNotFoundException {
+    int id = 999;
+    Student student = new Student();
+    student.setId(id);
+    List<StudentCourse> studentCourse = new ArrayList<>();
+    when(repository.searchStudent(id)).thenReturn(Optional.of((student)));
+    when(repository.searchStudentCourse(id)).thenReturn(studentCourse);
+
+    StudentDetail expected = new StudentDetail(student, new ArrayList<>());
+    StudentDetail actual = sut.searchStudent(id);
+
+    verify(repository, times(1)).searchStudent(id);
+    verify(repository, times(1)).searchStudentCourse(id);
+  }
+
+  @Test
+  void IDによる受講生詳細検索_IDが存在しない場合に例外処理が適切に呼び出せていること() {
+
+    MemberNotFoundException expected = assertThrows(MemberNotFoundException.class,
+        () -> sut.searchStudent(999));
+    MatcherAssert.assertThat(expected.getMessage(), CoreMatchers.is("member not found"));
+  }
+
+  @Test
+  void 受講生詳細登録_リポジトリの処理が適切に呼び出せていること() {
+    Student student = new Student();
+    StudentCourse studentCourse = new StudentCourse();
+    List<StudentCourse> studentCourseList = List.of(studentCourse);
+    StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
+
+    sut.registerStudent(studentDetail);
+
+    verify(repository, times(1)).registerStudent(student);
+    verify(repository, times(1)).registerStudentCourse(studentCourse);
+  }
+
+  @Test
+  void 受講生詳細登録_受講生コース情報の初期化処理が行われること()
+      throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    int id = 999;
+    Student student = new Student();
+    student.setId(id);
+    StudentCourse studentCourse = new StudentCourse();
+
+    //リフレクション
+    Method method = StudentService.class.getDeclaredMethod("initStudentsCourse",
+        StudentCourse.class,
+        int.class);
+    method.setAccessible(true);
+    method.invoke(sut, studentCourse, student.getId());
+
+    assertEquals(id, studentCourse.getStudentId());
+    assertEquals(LocalDateTime.now().getHour(), studentCourse.getCourseStartAt().getHour());
+    assertEquals(LocalDateTime.now().plusYears(1).getYear(),
+        studentCourse.getCourseEndAt().getYear());
+  }
+
+  @Test
+  void 受講生詳細更新_リポジトリの処理が適切に呼び出せていること() {
+    Student student = new Student();
+    StudentCourse studentCourse = new StudentCourse();
+    List<StudentCourse> studentCourseList = List.of(studentCourse);
+    StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
+
+    sut.updateStudent(studentDetail);
+
+    verify(repository, times(1)).updateStudent(student);
+    verify(repository, times(1)).updateStudentCourse(studentCourse);
+  }
 }
+
+
